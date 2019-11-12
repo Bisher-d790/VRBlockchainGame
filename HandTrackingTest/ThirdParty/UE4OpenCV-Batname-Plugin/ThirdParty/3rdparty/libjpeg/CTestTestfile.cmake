98621d61a8e2a931e3c58abe32bc9378768d11a8
Bisher-d790 <35769802+Bisher-d790@users.noreply.github.com>
2019-11-12 23:26:18 +0800

    Remove non-working resources

--- HandTrackingTest/ThirdParty/UE4OpenCV-Batname-Plugin/ThirdParty/3rdparty/libjpeg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/SDKs/opencv/3rdparty/libjpeg
-# Build directory: D:/SDKs/opencv/Build/3rdparty/libjpeg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- HandTrackingTest/ThirdParty/UE4OpenCV-Batname-Plugin/ThirdParty/3rdparty/openexr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/SDKs/opencv/3rdparty/openexr
-# Build directory: D:/SDKs/opencv/Build/3rdparty/openexr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
