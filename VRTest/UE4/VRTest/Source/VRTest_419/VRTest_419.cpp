18dfa6a67278f0bb4e4913f1d6ad2260b2edef46
Bisher-d790 <35769802+Bisher-d790@users.noreply.github.com>
2019-11-23 02:26:00 +0800

    Cleanup All Projects

--- VRTest/UE4/VRTest/Source/VRTest_419/VRTest_419.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "VRTest_419.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRTest_419, "VRTest_419" );
- 
